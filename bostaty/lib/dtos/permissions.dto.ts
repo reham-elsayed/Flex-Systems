91a6bde8734ef20991dc0351a287c8ce66bd7b14
reham-elsayed <rehamshipl445@gmail.com>
2026-04-06 08:46:28 +0200

    Fix: Flatten repository structure and move source code to root

--- bostaty/lib/dtos/permissions.dto.ts
+++
@@ -1,8 +0,0 @@
-import z from "zod";
-
-export const UserPermissionsSchema = z.object({
-    email: z.email(),
-    permissions: z.array(z.string()),
-});
-
-export type UserPermissionsDto = z.infer<typeof UserPermissionsSchema>;
